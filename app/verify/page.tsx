128795fe4073e344476c412ead585b168a172129
jatin <jatinranjan8@gmail.com>
2023-08-26 20:05:27 +0530

    app/verify/page.tsx saved , missed in last Commit

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { notFound } from "next/navigation";
 
-export default function Verify() {
+interface Props {
+  searchParams: { token: string; userId: string };
+}
+// every key:value pair after ? in url is stored in searchParams
+export default function Verify(props: Props) {
+  const { token, userId } = props.searchParams;
+
+  //verify the token ans userId
+
+  if (!token || !userId) return notFound();
+
   return (
     <div className="text-3xl opacity-70 text-center p-5 animate-pulse">
       Please wait...
