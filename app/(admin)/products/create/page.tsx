9ab210feb73f18347174b7aea66b5b0c85270f32
jatin <jatinranjan8@gmail.com>
2023-08-29 09:32:35 +0530

    Product Model done, created new product in MongoDb, L-72

--- app/(admin)/products/create/page.tsx
+++ app/(admin)/products/create/page.tsx
@@ -1,9 +1,48 @@
+"use client";
 import ProductForm from "@/app/components/ProductForm";
+import { NewProductInfo } from "@/app/types";
+import { uploadImage } from "@/app/utils/helper";
+import { newProductInfoSchema } from "@/app/utils/validationSchema";
 import React from "react";
+import { toast } from "react-toastify";
+import { ValidationError } from "yup";
+import { createProduct } from "../action";
 export default function Create() {
+  const handleCreateProduct = async (values: NewProductInfo) => {
+    const { thumbnail, images } = values;
+    try {
+      await newProductInfoSchema.validate(values, { abortEarly: false });
+
+      const thumbnailRes = await uploadImage(thumbnail!);
+
+      let productImages: { url: string; id: string }[] = [];
+
+      if (images) {
+        const uploadPromise = images.map(async (image) => {
+          const { id, url } = await uploadImage(image);
+          return { id, url };
+        });
+        productImages = await Promise.all(uploadPromise);
+      }
+
+      await createProduct({
+        ...values,
+        price: {
+          base: values.mrp,
+          discounted: values.salePrice,
+        },
+        thumbnail: thumbnailRes,
+        images: productImages,
+      });
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        error.inner.map((err) => toast.error(err.message));
+      }
+    }
+  };
   return (
     <div>
-      <ProductForm />
+      <ProductForm onSubmit={handleCreateProduct} />
     </div>
   );
 }
